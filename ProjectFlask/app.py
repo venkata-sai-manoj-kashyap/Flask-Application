e52c7c63f108b4050724ed21c80f9a500a7959f6
Manoj Kashyap <kashyap_matukumalli@yahoo.co.in>
2019-03-09 01:19:20 -0500

    inital commit

---
+++ ProjectFlask/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/home.htmla')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
